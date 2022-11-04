b9b0a2ddd40533890251e2bc390017c4f4ae5576
Robin <robin.morand@gmail.com>
2022-11-04 20:20:30 +0100

    add Tp02_ex05 use of list

--- Python/tp02/TP02_05.py
+++ Python/tp02/TP02_05.py
@@ -1,44 +1,41 @@
 import random
 
 saved_value: int = 0
-dice1: int = 0
-dice2: int = 0
-dice3: int = 0
+dices: list = list((0, 0, 0))
 
 
 def launch_dice(dice_value):
-    return random.randint(1, 6) if (dice_value != saved_value or saved_value == 0) else saved_value
+    return random.randint(6, 6) if (dice_value != saved_value or saved_value == 0) else saved_value
 
 
 def print_dice():
-    print('|', dice1, '| ', '|', dice2, '| ', '|', dice3, '|')
+    print('|', dices[0], '| ', '|', dices[1], '| ', '|', dices[2], '|')
 
 
 def calc_score():
-    if dice1 == dice2 == dice3:
-        if dice1 == 1:
+    if dices[0] == dices[1] == dices[2]:
+        if 1 in dices:
             return 100
-        if dice1 == 6:
+        if 6 in dices:
             return 60
-        return dice1
+        return dices[0]
     return 0
 
 
 total_score: int = 0
 while total_score < 200:
     user_entry: str = "0"
-    dice_to_launch: int = 3
-    round_current: int = 1
     saved_value = 0
 
     while user_entry != "":
         user_entry = input("Appuyer sur \"Enter\" pour lancer les dés")
-    for round_current in range(1, 4):
-        dice1 = launch_dice(dice1)
-        dice2 = launch_dice(dice2)
-        dice3 = launch_dice(dice3)
+    for current_round in range(1, 4):
+        for index, dice in enumerate(dices):
+            dices[index] = launch_dice(dice)
         print_dice()
-        while round_current < 3:
+        if dices[0] == dices[1] == dices[2]:
+            break
+        while current_round < 3:
             user_entry = input("Voulez-vous garder une valeur ? (1-6)")
             if user_entry == "":
                 saved_value = 0
@@ -47,4 +44,4 @@ while total_score < 200:
                 saved_value = int(user_entry)
                 break
     total_score += calc_score()
-    print("Votre score est : {t} pts".format(t=total_score))
+    print("Votre score est : ", total_score, "pts")
