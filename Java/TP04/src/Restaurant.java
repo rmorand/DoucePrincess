b00fc0856bccd90ac0ecb5558fc70a9f6648769f
Robin <robin.morand@gmail.com>
2024-05-12 05:47:34 +0200

    TP04 : add

---
+++ Java/TP04/src/Restaurant.java
@@ -0,0 +1,21 @@
+public class Restaurant
+{
+    public static void main(String[] args)
+    {
+        Plat p1 = new Plat("Cassoulet", "Plat");
+        Plat p2 = new Plat("Pâtes au saumon", "Plat");
+        Plat p3 = new Plat("Salade verte", "Entrée");
+        Plat p4 = new Plat("Assiette Valaisanne", "Entrée/Plat");
+        Plat p5 = new Plat("Coupe Danemark", "Dessert");
+        Plat p6 = new Plat("Quiche Lorraine", "Plat");
+        Plat p7 = new Plat("Pizza 4 saisons", "Plat");
+        Plat p8 = new Plat("Tartare d'aubergines", "Entrée");
+        Plat p9 = new Plat("Salade Niçoise", "Entrée/Plat");
+        Plat p10 = new Plat("Profiteroles", "Dessert");
+        Menu m1 = new Menu("du jour", 35);
+        Menu m2 = new Menu("de saison", 40);
+        Menu m3 = new Menu("duo", 60);
+        Menu m4 = new Menu("enfant", 25);
+        Menu m5 = new Menu("découverte", 45);
+    }
+}
