0fb72d301b5b72937f475d5ddd1d489f5798d732
Robin <robin.morand@gmail.com>
2024-05-20 18:26:38 +0200

    Revert "TP10 : add Solution Robin"
    This reverts commit 76f990b00cda8dfbaf935cb0ccf7366637b4a613.

--- Java/TP10_2024_enonce/src/Moto.java
+++ Java/TP10_2024_enonce/src/Moto.java
@@ -1,17 +1,3 @@
-public class Moto extends Vehicule {
-    final int cylindree;
+public class Moto {
 
-    public Moto(String immatriculation, String marque, String modele, String etat, int cylindree){
-        super(immatriculation, marque, modele, etat);
-        this.cylindree = cylindree;
-    }
-
-    public int getCylindree() {
-        return cylindree;
-    }
-
-    @Override
-    public String toString() {
-        return "Moto "  + this.getCylindree() + "cc : " + this.getMarque() + " " + this.getModele() + " immatriculée : " + this.getImmatriculation() + ".";
-    }
 }
--- Java/TP10_2024_enonce/src/Vehicule.java
+++ Java/TP10_2024_enonce/src/Vehicule.java
@@ -1,36 +1,5 @@
 import java.util.Objects;
 
-public abstract class Vehicule {
-
-    private final String immatriculation;
-    private final String marque;
-    private final String modele;
-    private final String etat;
-
-    public Vehicule(String immatriculation, String marque, String modele, String etat) {
-        this.immatriculation = immatriculation;
-        this.marque = marque;
-        this.modele = modele;
-        this.etat = etat;
-    }
-
-    public String getType() {
-        return this.getClass().getName();
-    }
-
-    public String getImmatriculation() {
-        return immatriculation;
-    }
-
-    public String getMarque() {
-        return marque;
-    }
-
-    public String getModele() {
-        return modele;
-    }
-
-    public String getEtat() {
-        return etat;
-    }
+public class Vehicule {
+    
 }
